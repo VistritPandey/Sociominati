918a6059e5abf5a9c1c4e6515189ffcaafcd0ee6
vistritpandey <vistritpandey@gmail.com>
2021-01-02 08:54:27 +0530

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import Feed from "./Feed";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import Login from "./Login";
+import { login, logout } from "./features/userSlice";
+import { auth } from "./Firebase";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            id: authUser.id,
+            photoUrl: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <div className="app__body"></div>
-    </div>
+    <>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="App">
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
