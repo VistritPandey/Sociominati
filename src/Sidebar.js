8f056e5ed214b6fbf33820345b4f54e6db4426e0
vistritpandey <vistritpandey@gmail.com>
2021-01-02 08:45:37 +0530

    Sidebar added

---
+++ src/Sidebar.js
@@ -0,0 +1,57 @@
+import { Avatar, Button } from "@material-ui/core";
+import React from "react";
+import "./Sidebar.css";
+import { auth } from "./Firebase";
+import { selectUser } from "./features/userSlice";
+import { useSelector } from "react-redux";
+
+function Sidebar() {
+  const user = useSelector(selectUser);
+
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p href="https://github.com/VistritPandey/Gmail-Clone">{topic}</p>
+    </div>
+  );
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img
+          src="https://www.costacruises.com/content/dam/costa/inventory-assets/countries/IND/IND.jpg.image.750.563.low.jpg"
+          alt=""
+        />
+        <Avatar className="sidebar__avatar" src={user.photoUrl} />
+        <h2>{user.displayName}</h2>
+        <h4>{user.email}</h4>
+      </div>
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who Viewed you</p>
+          <p className="sidebar__statNumber">2,303</p>
+        </div>
+        <div className="sidebar__stat">
+          <p>Views on Post</p>
+          <p className="sidebar__statNumber">2,323</p>
+        </div>
+      </div>
+      <div className="sidebar__bottom">
+        <p>Repo Link</p>
+        {recentItem("Github Repo")}
+      </div>
+      <div className="sidebar__logout">
+        <Button
+          onClick={() => {
+            auth.signOut();
+          }}
+        >
+          {" "}
+          Logout{" "}
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
