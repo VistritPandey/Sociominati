264cd09aa555ee6434758ac8ecab6d2d1f03b5c8
vistritpandey <vistritpandey@gmail.com>
2021-01-02 08:36:31 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="app__body"></div>
+    </div>
+  );
 }
 
 export default App;
--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -6,10 +6,6 @@ export const userSlice = createSlice({
     user: null,
   },
   reducers: {
-    // Redux Toolkit allows us to write "mutating" logic in reducers. It
-    // doesn't actually mutate the state because it uses the Immer library,
-    // which detects changes to a "draft state" and produces a brand new
-    // immutable state based off those changes
     login: (state, action) => {
       state.user = action.payload;
     },
@@ -19,9 +15,6 @@ export const userSlice = createSlice({
   },
 });
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.counter.value)`
 export const { login, logout } = userSlice.actions;
 
 export const selectUser = (state) => state.user.user;
