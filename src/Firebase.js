a30a5b5efface62070d8f041b90f2eb7be2a81f3
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2021-02-01 08:17:17 -0500

    Config settings removed

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCgAGXZf_qvZ_vOjq1cDxmm-XhAMLCISxw",
-  authDomain: "sociominati.firebaseapp.com",
-  projectId: "sociominati",
-  storageBucket: "sociominati.appspot.com",
-  messagingSenderId: "110797338899",
-  appId: "1:110797338899:web:c92b5d8eff266a7eabc2e6",
-  measurementId: "G-QDPET77XZ0",
+  // add your own config file
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
